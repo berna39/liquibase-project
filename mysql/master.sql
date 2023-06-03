70a8a6277caf21795b703df98bff381dcbbb3808
berna39 <kalemajoseph39@gmail.com>
2023-06-03 21:58:48 +0200

    :sparkles: changelog rollbacks

--- mysql/master.sql
+++ mysql/master.sql
@@ -1,8 +1,14 @@
 -- liquibase formatted sql
 
---changeset terminato:1
+--changeset terminator:1
 create table liquibase.students(
     id int primary key,
     noms varchar(191)
 );
 
+--rollback drop table students
+
+--changeset shango:2
+ALTER table liquibase.students change noms name varchar(191)
+
+--rollback liquibase.students change name noms varchar(191)
